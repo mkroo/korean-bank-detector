0895cf520bde39da14fc319db9910f6f8b97d31e
ansrl0107 <ansrl0107@gmail.com>
2026-05-09 12:13:46 +0900

    chore: add eslint, prettier, editorconfig

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  ignorePatterns: ['dist/', 'node_modules/', 'src/data/logos.ts', 'src/logos/'],
+  rules: {
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+}
